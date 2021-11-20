835cc9ebf883f1f9cdaad7e6dbd9865f4cf7f6d0
mediazjr <91097193+mediazjr@users.noreply.github.com>
2021-11-19 18:25:48 -0800

    added div with h1 and p elements to index.html

--- assests/script.js
+++ assests/script.js
@@ -1,5 +1,16 @@
-// questions array of objects with each question, its options, and answers
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
 
+// questions array of objects with each question, its options, and answers
 var questions = [
 
     {
@@ -38,3 +49,10 @@ var questions = [
     }
 
 ];
+
+// variables for timer
+var timeLeft = document.querySelector("#timeLeft");
+var timer = document.querySelector("startTimer")
+
+
+// start button add event listener
