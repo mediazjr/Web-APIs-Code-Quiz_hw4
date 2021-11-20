68ddf056ab3237421730811134810b71519607e0
mediazjr <91097193+mediazjr@users.noreply.github.com>
2021-11-19 18:02:59 -0800

    added var with questions in javascript

---
+++ assests/script.js
@@ -0,0 +1,40 @@
+// questions array of objects with each question, its options, and answers
+
+var questions = [
+
+    {
+        question: "Which of these can be stored in an Array?",
+        options: ["numbers", "booleans", "strings", "all of the above"],
+        answer: "all of the above"
+
+    }
+
+    {
+        question: "What is the best Method for debugging syntax errors in code?",
+        options: ["console.log()", "git pull", "function ()", "for loop"],
+        answer: "console.log()"
+
+    }
+
+    {
+        question: "What are the most common declared Javascript variables?",
+        options: ["var", "let", "const", "all of the above"],
+        answer: "all of the above"
+
+    }
+
+    {
+        question: "Commonly used data types DO NOT include:",
+        options: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+
+    }
+
+    {
+        question: "The condition in an if / else statement is enclosed within ____. ",
+        options: ["quotes", "curley brackets", "square brackets", "parenthesis"],
+        answer: "quotes"
+
+    }
+
+];
